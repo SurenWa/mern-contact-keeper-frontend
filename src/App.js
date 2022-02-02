dc6b87726fb65607615bea65835d08f519fbea87
unknown <cirrusminor2@gmail.com>
2022-02-02 19:44:03 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
+import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
+
+import NavBar from './components/layout/NavBar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
+import PrivateRoute from './components/routing/PrivateRoute';
+
+if (localStorage.token) {
+    setAuthToken(localStorage.token)
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <AuthState>
+            <ContactState>
+                <AlertState>
+                    <BrowserRouter>
+                        <>
+                            <NavBar />
+                            <div className="container">
+                                <Alerts />
+                                <Routes>
+                                    <Route
+                                        path="/"
+                                        element={
+                                            <PrivateRoute>
+                                                <Home />
+                                            </PrivateRoute>
+                                        }
+                                    />
+                                    <Route exact path="/about" element={<About />} />
+                                    <Route exact path="/register" element={<Register />} />
+                                    <Route exact path="/login" element={<Login />} />
+                                </Routes>
+                            </div>
+                        </>
+                    </BrowserRouter>
+                </AlertState>
+            </ContactState>
+        </AuthState>
+
+    );
 }
 
 export default App;
+
+
+
+// "eslintConfig": {
+//   "extends": [
+//     "react-app",
+//     "react-app/jest"
+//   ]
+// },
